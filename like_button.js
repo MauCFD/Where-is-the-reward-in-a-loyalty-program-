3d03c7a15df197b5e49bcadd101908874008e4fe
carlosalvarezdeloscobos <33360614+carlosalvarezdeloscobos@users.noreply.github.com>
2021-05-17 20:26:07 -0500

    react starter code

---
+++ like_button.js
@@ -0,0 +1,29 @@
+// React starter code
+// https://reactjs.org/docs/add-react-to-a-website.html
+
+
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}// ... the starter code you pasted ...
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
